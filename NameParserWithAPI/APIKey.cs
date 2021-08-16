115bc13b917a5176455ce668e8d4a4a5f3d08077
nicomp42 <nicholdw@ucmail.uc.edu>
2021-08-16 08:27:19 -0400

    Mod'd to lok for the API Key file in the same folder as the project. This is waaay better than folder where the executable is, which could change based on Debug/Release mode.

--- NameParserWithAPI/APIKey.cs
+++ NameParserWithAPI/APIKey.cs
@@ -14,9 +14,9 @@ namespace NameParserWithAPI
             {
                 /// OMG. https://stackoverflow.com/questions/837488/how-can-i-get-the-applications-path-in-a-net-console-application
                 /// This works differently for WinForm and Console apps. Yikes.
-                apiKey = System.IO.File.ReadAllText(Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), apiKeyFile));
-            } catch (Exception ex)
-            {
+                /// apiKey = System.IO.File.ReadAllText(Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), apiKeyFile));
+                apiKey = System.IO.File.ReadAllText("..\\..\\..\\"+ apiKeyFile);
+            } catch (Exception ex) {
                 Console.WriteLine("APIKey.read() : " + ex.Message);
                 //apiKey = "Unable to read API Key from " + apiKeyFile;
             }
